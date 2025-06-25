62198789850f85f57dae6fd235fd69c94f32654f
Mohan Gupta <mohangupta@live.com>
2025-06-25 17:12:34 +1000

    next collection date

--- app/src/main/java/com/melbpc/mohankumargupta/helloworldtv/HomeScreen.kt
+++ app/src/main/java/com/melbpc/mohankumargupta/helloworldtv/HomeScreen.kt
@@ -12,9 +12,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.temporal.TemporalAdjusters
 
 //import androidx.tv.material3.Text
+
+fun findNextDayOfWeek(targetDay: DayOfWeek, fromDate: LocalDate = LocalDate.now()): LocalDate {
+    return fromDate.with(TemporalAdjusters.next(targetDay))
+}
 
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
@@ -22,12 +28,20 @@ fun HomeScreen(viewModel: MainViewModel) {
     val collectionDay = viewModel.collectionDay.collectAsState().value
     val recyclingBinColor = viewModel.recyclingBinColor.collectAsState().value
     val gardenBinColor = viewModel.gardenBinColor.collectAsState().value
+    val recyclingReferenceDate = viewModel.recyclingReferenceDate.collectAsState().value
 
     val today = LocalDate.now()
     val isCollectionDayToday = collectionDay.uppercase() == today.dayOfWeek.name
-    Log.d("mohan", today.dayOfWeek.name)
-    Log.d("mohan", collectionDay.uppercase())
-    Log.d("mohan", isCollectionDayToday.toString())
+//    Log.d("mohan", today.dayOfWeek.name)
+//    Log.d("mohan", collectionDay.uppercase())
+//    Log.d("mohan", isCollectionDayToday.toString())
+    if (recyclingReferenceDate?.isBefore(today) == true) {
+        var nextCollectionDate: LocalDate = if (isCollectionDayToday) today else findNextDayOfWeek(DayOfWeek.valueOf(collectionDay.uppercase()))
+        Log.d("mohan", nextCollectionDate.toString())
+    }
+
+
+
 
     LazyColumn(
         modifier = Modifier.fillMaxSize(),
